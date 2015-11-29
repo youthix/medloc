77c3abba5808d75a4d1d00221be66ad7f0e24fdd
Saurabh Dixit <saurabh.iips@gmail.com>
2015-11-29 15:03:28 +0530

    Added

---
+++ src/entities/ListLocationDetail.java
@@ -0,0 +1,20 @@
+package entities;
+
+import java.util.List;
+
+public class ListLocationDetail {
+	private List <LocationDetail> locationDetail ;
+
+	public List<LocationDetail> getLocationDetail() {
+		return locationDetail;
+	}
+
+	public void setLocationDetail(List<LocationDetail> locationDetail) {
+		this.locationDetail = locationDetail;
+	}
+
+	
+	
+
+
+}
