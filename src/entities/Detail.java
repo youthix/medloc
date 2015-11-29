7c553a89ea7692e6795580b6a82338d6c637678f
Saurabh Dixit <saurabh.iips@gmail.com>
2015-11-29 19:48:15 +0530

    Added

---
+++ src/entities/Detail.java
@@ -0,0 +1,116 @@
+package entities;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "Detail")
+public class Detail {
+	
+    private String id;
+    private String name;
+    private String logo;
+    private String url;
+    private String pinCode; 
+    private String batchNo;
+    private String expiryDate;
+    private String type;
+    private String quantity;
+    private String companyID;
+    private String companyName;
+    private String address;
+    private String contactNumber;
+    private String contactPerson;
+    private String emailID;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLogo() {
+		return logo;
+	}
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(String pinCode) {
+		this.pinCode = pinCode;
+	}
+	public String getBatchNo() {
+		return batchNo;
+	}
+	public void setBatchNo(String batchNo) {
+		this.batchNo = batchNo;
+	}
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	public String getCompanyID() {
+		return companyID;
+	}
+	public void setCompanyID(String companyID) {
+		this.companyID = companyID;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public String getContactPerson() {
+		return contactPerson;
+	}
+	public void setContactPerson(String contactPerson) {
+		this.contactPerson = contactPerson;
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+
+    
+
+}
---
+++ src/entities/ListDetailVO.java
@@ -0,0 +1,21 @@
+package entities;
+
+import java.util.List;
+
+public class ListDetailVO {
+	private List <Detail> Detail ;
+
+	public List<Detail> getDetail() {
+		return Detail;
+	}
+
+	public void setDetail(List<Detail> detail) {
+		Detail = detail;
+	}
+
+
+	
+	
+
+
+}
