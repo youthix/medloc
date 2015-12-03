3f4ef3f67a1037c47a06f57c7fcafb8e6e2316ef
Saurabh Dixit <saurabh.iips@gmail.com>
2015-12-03 19:40:58 +0530

    Updated

--- src/entities/UserInfo.java
+++ src/entities/UserInfo.java
@@ -5,13 +5,16 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "userInfo")
 public class UserInfo
 {
-
-    private String userName;
-    private String password;
-    private String userID;
-    private String role;
-    private String lastLoginTimeStamp;
-    
+    private String userName="";
+    private String password="";
+    private String userID="";
+    private String role="";
+    private String lastLoginTimeStamp="";
+    private String name="";
+    private String address="";
+    private String mobileNumber="";
+    private String email="";
+    private String alternateNumber="";
 
     public UserInfo()
     {
@@ -64,6 +67,45 @@ public class UserInfo
 	public void setLastLoginTimeStamp(String lastLoginTimeStamp) {
 		this.lastLoginTimeStamp = lastLoginTimeStamp;
 	}
-    
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAlternateNumber() {
+		return alternateNumber;
+	}
+
+	public void setAlternateNumber(String alternateNumber) {
+		this.alternateNumber = alternateNumber;
+	}    
     
 }
