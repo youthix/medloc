d42952b4f0f02824dfa9748891256fbac9fe569b
Saurabh Dixit <saurabh.iips@gmail.com>
2015-12-04 00:37:37 +0530

    Added

---
+++ src/entities/BootRequest.java
@@ -0,0 +1,35 @@
+package entities;
+
+public class BootRequest {
+	
+	public String uuid;
+	public String token;
+	public String platform;
+	public String appId;
+	
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getPlatform() {
+		return platform;
+	}
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
+	public String getAppId() {
+		return appId;
+	}
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+
+}
