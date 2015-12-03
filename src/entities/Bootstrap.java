18f92aa1f6f4fa46c7abc11963a90b6857a1c184
Saurabh Dixit <saurabh.iips@gmail.com>
2015-12-03 22:31:36 +0530

    Updated

---
+++ src/entities/Bootstrap.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Bootstrap{
+	
+	public String promotionArticle="<html><body><center><img src='http://2.bp.blogspot.com/-G1vh_uLbyM8/U5W__nOU6eI/AAAAAAAAIIg/v1y8V_TEduI/s1600/coffee-smiley.png' height='90' width='90'/><br><p>Welcome, Let us help you in finding your medicine. <br> Meanwhile you can have your coffee too. <br> Thanks !! </p><center></body></html>";
+	public String defLoc="";
+	public String getPromotionArticle() {
+		return promotionArticle;
+	}
+	public void setPromotionArticle(String promotionArticle) {
+		this.promotionArticle = promotionArticle;
+	}
+	public String getDefLoc() {
+		return defLoc;
+	}
+	public void setDefLoc(String defLoc) {
+		this.defLoc = defLoc;
+	}	
+}
