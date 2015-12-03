a029c1c4a1ab54431e577d760c8c9620926643cd
Saurabh Dixit <saurabh.iips@gmail.com>
2015-12-04 00:53:59 +0530

    Updated

--- src/entities/Bootstrap.java
+++ src/entities/Bootstrap.java
@@ -3,7 +3,7 @@ package entities;
 public class Bootstrap{
 	
 	public String promotionArticle="<html><body><center><img src='http://2.bp.blogspot.com/-G1vh_uLbyM8/U5W__nOU6eI/AAAAAAAAIIg/v1y8V_TEduI/s1600/coffee-smiley.png' height='90' width='90'/><br><p>Welcome, Let us help you in finding your medicine. <br> Meanwhile you can have your coffee too. <br> Thanks !! </p><center></body></html>";
-	public String defLoc="";
+	public String defLoc="loc2";
 	public String getPromotionArticle() {
 		return promotionArticle;
 	}
