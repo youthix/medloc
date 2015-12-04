0e1790b13e189c9ff8c0f969dd76a624b80831c3
Saurabh Dixit <saurabh.iips@gmail.com>
2015-12-04 23:03:20 +0530

    Updated

---
+++ src/entities/Preferences.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Preferences {
+	
+	public String location="";
+	public String emailNotify="";
+	
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getEmailNotify() {
+		return emailNotify;
+	}
+	public void setEmailNotify(String emailNotify) {
+		this.emailNotify = emailNotify;
+	}	
+
+}
