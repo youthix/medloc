723c1ade335d063193777dcf2345f2c3b5f0edf6
Saurabh Dixit <saurabh.iips@gmail.com>
2015-12-05 12:37:09 +0530

    Updated

--- src/entities/Bootstrap.java
+++ src/entities/Bootstrap.java
@@ -4,6 +4,9 @@ public class Bootstrap{
 	
 	public String promotionArticle="<html><body><center><img src='http://2.bp.blogspot.com/-G1vh_uLbyM8/U5W__nOU6eI/AAAAAAAAIIg/v1y8V_TEduI/s1600/coffee-smiley.png' height='90' width='90'/><br><p>Welcome, Let us help you in finding your medicine. <br> Meanwhile you can have your coffee too. <br> Thanks !! </p><center></body></html>";
 	public String defLoc="loc2";
+	public String faceUrl="https://www.facebook.com/";
+	public String tweetUrl="https://twitter.com/";
+	public String instaUrl="https://www.instagram.com/";
 	public String getPromotionArticle() {
 		return promotionArticle;
 	}
@@ -15,5 +18,23 @@ public class Bootstrap{
 	}
 	public void setDefLoc(String defLoc) {
 		this.defLoc = defLoc;
-	}	
+	}
+	public String getFaceUrl() {
+		return faceUrl;
+	}
+	public void setFaceUrl(String faceUrl) {
+		this.faceUrl = faceUrl;
+	}
+	public String getTweetUrl() {
+		return tweetUrl;
+	}
+	public void setTweetUrl(String tweetUrl) {
+		this.tweetUrl = tweetUrl;
+	}
+	public String getInstaUrl() {
+		return instaUrl;
+	}
+	public void setInstaUrl(String instaUrl) {
+		this.instaUrl = instaUrl;
+	}		
 }
