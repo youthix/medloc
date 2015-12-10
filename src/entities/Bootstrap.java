9cb08bee5aa5be88636fe23262f3a8d46acb8f43
Saurabh Dixit <saurabh.iips@gmail.com>
2015-12-10 22:51:13 +0530

    Updated

--- src/entities/Bootstrap.java
+++ src/entities/Bootstrap.java
@@ -3,11 +3,14 @@ package entities;
 public class Bootstrap{
 	
 	public String promotionArticle="<html><body><center><img src='http://2.bp.blogspot.com/-G1vh_uLbyM8/U5W__nOU6eI/AAAAAAAAIIg/v1y8V_TEduI/s1600/coffee-smiley.png' height='90' width='90'/><br><p>Welcome, Let us help you in finding your medicine. <br> Meanwhile you can have your coffee too. <br> Thanks !! </p><center></body></html>";
+	public String aboutUsArticle="<html><body><center><img src='http://2.bp.blogspot.com/-G1vh_uLbyM8/U5W__nOU6eI/AAAAAAAAIIg/v1y8V_TEduI/s1600/coffee-smiley.png' height='90' width='90'/><br><p>Welcome, Let us help you in finding your medicine. <br> Meanwhile you can have your coffee too. <br> Thanks !! </p><center></body></html>";
 	public String defLoc="loc2";
 	public String faceUrl="https://www.facebook.com/";
 	public String tweetUrl="https://twitter.com/";
 	public String instaUrl="https://www.instagram.com/";
 	public String linkedUrl="https://in.linkedin.com/";
+	public String mailTo="medlocbox@gmail.com,scribbletechnologies@gmail.com";
+	public String mailSub="What's your say ? We are hearing !";
 	public String getPromotionArticle() {
 		return promotionArticle;
 	}
@@ -43,5 +46,23 @@ public class Bootstrap{
 	}
 	public void setLinkedUrl(String linkedUrl) {
 		this.linkedUrl = linkedUrl;
+	}
+	public String getAboutUsArticle() {
+		return aboutUsArticle;
+	}
+	public void setAboutUsArticle(String aboutUsArticle) {
+		this.aboutUsArticle = aboutUsArticle;
+	}
+	public String getMailTo() {
+		return mailTo;
+	}
+	public void setMailTo(String mailTo) {
+		this.mailTo = mailTo;
+	}
+	public String getMailSub() {
+		return mailSub;
+	}
+	public void setMailSub(String mailSub) {
+		this.mailSub = mailSub;
 	}	
 }
